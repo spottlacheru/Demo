cd66ba1445852372fa4d9b5b0ac178ea47e11514
Sunil Pottlacheru <spottlacheru@nimble.com.au>
2017-08-15 14:58:52 +1000

    Updated Core Framework

---
+++ Toyota.Automation/Feature/VerifyAppContent.feature
@@ -0,0 +1,22 @@
+﻿Feature: Content
+	Verify the display content in all the web pages
+
+@smoke_test
+Scenario: Verify display content
+	Given The User has Launched URL
+	When The User click on "Home" button
+	Then The Page Navigates to "Home" page
+    And The User Sees "Display" image
+    And The User Sees "Details" table
+
+    
+@smoke_test
+@Browser:Chrome
+Scenario: Verify login
+	Given The User has Launched URL
+	When The User click on "Login" button
+	Then The Page Navigates to "Login" page
+    And The User Sees "UserName" button
+    And The User Sees "Password" button
+    Then The User enters "" in "" entry
+    Then The User enters "" in "" entry
---
+++ Toyota.Automation/Steps/ContentSteps.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace Toyota.Automation
+{
+    [Binding]
+    public class ContentSteps
+    {
+        private IWebDriver _driver;
+
+        public ContentSteps(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+
+        [Given(@"The User has Launched URL")]
+        public void GivenTheUserHasLaunchedURL()
+        {
+            _driver.Navigate().GoToUrl("https://www.google.com.au");
+            System.Threading.Thread.Sleep(30000);
+        }
+
+        [When(@"The User click on ""(.*)"" button")]
+        public void WhenTheUserClickOnButton(string p0)
+        {
+           
+        }
+
+        [Then(@"The Page Navigates to ""(.*)"" page")]
+        public void ThenThePageNavigatesToPage(string p0)
+        {
+           
+        }
+
+        [Then(@"The User Sees ""(.*)"" image")]
+        public void ThenTheUserSeesImage(string p0)
+        {
+           
+        }
+
+        [Then(@"The User Sees ""(.*)"" table")]
+        public void ThenTheUserSeesTable(string p0)
+        {
+           
+        }
+
+        [Then(@"The User Sees ""(.*)"" button")]
+        public void ThenTheUserSeesButton(string p0)
+        {
+           
+        }
+
+        [Then(@"The User enters """"(.*)"""" entry")]
+        public void ThenTheUserEntersEntry(string p0)
+        {
+           
+        }
+
+
+    }
+}
