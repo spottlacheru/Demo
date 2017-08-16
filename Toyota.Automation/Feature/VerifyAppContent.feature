b6583cd299dc248e79fa71e8b888166874f88bd4
Purna <purna.mutyala@cigniti.com>
2017-08-16 18:02:13 +0530

    [ci skip] added contentsteps

--- Toyota.Automation/Feature/VerifyAppContent.feature
+++ Toyota.Automation/Feature/VerifyAppContent.feature
@@ -20,3 +20,43 @@ Scenario: Verify login
     And The User Selects LoanAmount slider
     Then The User enters LoanAmount entry
     Then The User click on Continue button
+
+	@Smoke_test
+	Scenario: : Nimble
+	Given Navigate to URL
+Then Click on Apply button.
+And Click on Start application button.
+And Select loan Amount (MAAC-3000)
+And Select purpose of loan (Households)
+And Enter purpose of loan amount (MAAC-3000)
+And Enter Personal Details & contact details
+And Select bank (Dag bank).
+And Enter bank credentials 
+And Choose bank account
+And Enter bank details (BSB- 012004 , Account no 123456789 - ,Account name- TestUser)
+And Select Income Category (Primary income)
+And Select Other Debt Repayments Option as (No)
+And Select Dependents List as (0)
+And Click Govt. Benefits Options List as (NO)
+And Click Agree App Submit Button
+And Verify SMS OTP.
+And Verify loan Amount with Approved amount
+And Choose Loan Amount Slider (950)
+And Choose Frequency (Fortnightly)
+And Change First Repayment Date (loanAmount)
+And Move Slider Middle Amount RL (180)
+And Click Detailed Repayment Schedule.
+And Verify first date
+And Verify last Date
+And Verify repayment Amount
+And Verify no of repayments count
+And Verify repayment schedule amount
+And Getting a list of repayment count.
+And Select Spend less Reason
+And Click on Loan contract
+And Click on Confirm Accepting Contract
+And Click On Agree Button
+And Click on No-thanks Button
+And Click on Loan Dashboard Button
+And Click on Logout Button
+And Click on Logout Button
--- Toyota.Automation/Steps/ContentSteps.cs
+++ Toyota.Automation/Steps/ContentSteps.cs
@@ -121,6 +121,242 @@ namespace Toyota.Automation
         {
             ScenarioContext.Current.Pending();
         }
+        //--------------------------------indraja----------------//
+        
+
+        
+
+        
+
+       
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        
+
+        [Given(@"Navigate to URL")]
+        public void GivenNavigateToURL()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Apply button\.")]
+        public void ThenClickOnApplyButton_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Start application button\.")]
+        public void ThenClickOnStartApplicationButton_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select loan Amount \(MAAC(.*)\)")]
+        public void ThenSelectLoanAmountMAAC(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select purpose of loan \(Households\)")]
+        public void ThenSelectPurposeOfLoanHouseholds()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Enter purpose of loan amount \(MAAC(.*)\)")]
+        public void ThenEnterPurposeOfLoanAmountMAAC(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Enter Personal Details & contact details")]
+        public void ThenEnterPersonalDetailsContactDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select bank \(Dag bank\)\.")]
+        public void ThenSelectBankDagBank_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Enter bank credentials")]
+        public void ThenEnterBankCredentials()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Choose bank account")]
+        public void ThenChooseBankAccount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Enter bank details \(BSB- (.*) , Account no (.*) - ,Account name- TestUser\)")]
+        public void ThenEnterBankDetailsBSB_AccountNo_AccountName_TestUser(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select Income Category \(Primary income\)")]
+        public void ThenSelectIncomeCategoryPrimaryIncome()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select Other Debt Repayments Option as \(No\)")]
+        public void ThenSelectOtherDebtRepaymentsOptionAsNo()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select Dependents List as \((.*)\)")]
+        public void ThenSelectDependentsListAs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click Govt\. Benefits Options List as \(NO\)")]
+        public void ThenClickGovt_BenefitsOptionsListAsNO()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click Agree App Submit Button")]
+        public void ThenClickAgreeAppSubmitButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify SMS OTP\.")]
+        public void ThenVerifySMSOTP_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify loan Amount with Approved amount")]
+        public void ThenVerifyLoanAmountWithApprovedAmount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Choose Loan Amount Slider \((.*)\)")]
+        public void ThenChooseLoanAmountSlider(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Choose Frequency \(Fortnightly\)")]
+        public void ThenChooseFrequencyFortnightly()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Change First Repayment Date \(loanAmount\)")]
+        public void ThenChangeFirstRepaymentDateLoanAmount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Move Slider Middle Amount RL \((.*)\)")]
+        public void ThenMoveSliderMiddleAmountRL(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click Detailed Repayment Schedule\.")]
+        public void ThenClickDetailedRepaymentSchedule_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify first date")]
+        public void ThenVerifyFirstDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify last Date")]
+        public void ThenVerifyLastDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify repayment Amount")]
+        public void ThenVerifyRepaymentAmount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify no of repayments count")]
+        public void ThenVerifyNoOfRepaymentsCount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Verify repayment schedule amount")]
+        public void ThenVerifyRepaymentScheduleAmount()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Getting a list of repayment count\.")]
+        public void ThenGettingAListOfRepaymentCount_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Select Spend less Reason")]
+        public void ThenSelectSpendLessReason()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Loan contract")]
+        public void ThenClickOnLoanContract()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Confirm Accepting Contract")]
+        public void ThenClickOnConfirmAcceptingContract()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click On Agree Button")]
+        public void ThenClickOnAgreeButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on No-thanks Button")]
+        public void ThenClickOnNo_ThanksButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Loan Dashboard Button")]
+        public void ThenClickOnLoanDashboardButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Click on Logout Button")]
+        public void ThenClickOnLogoutButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
 
 
     }
