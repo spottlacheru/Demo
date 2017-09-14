6e532a599b5f4d303136d46d9dfc23cc8ee05da4
Purna <purna.mutyala@cigniti.com>
2017-09-14 09:23:59 +0530

    [ci skip] updated & executed in local chrome

--- Smartline.Automation/Properties/AssemblyInfo.cs
+++ Smartline.Automation/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Toyota.Automation")]
+[assembly: AssemblyTitle("Smartline.Automation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Toyota.Automation")]
+[assembly: AssemblyProduct("Smartline.Automation")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
