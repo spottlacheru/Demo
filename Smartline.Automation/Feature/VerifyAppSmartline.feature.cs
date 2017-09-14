6715f4558f3861645680985fc4078dc90930de7b
Purna <purna.mutyala@cigniti.com>
2017-09-14 09:44:38 +0530

    [ci skip] Test chrome parallel execution

--- Smartline.Automation/Feature/VerifyAppSmartline.feature.cs
+++ Smartline.Automation/Feature/VerifyAppSmartline.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Smartline.Automation.Feature
+namespace Toyota.Automation.Feature
 {
     using TechTalk.SpecFlow;
     
